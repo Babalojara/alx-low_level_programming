185636bc2dc5a1a5055d9c6f333584a62bf46ac9
Babalo Jara <jarathedawg@gmail.com>
2023-02-23 20:27:27 -0800

    Babalo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
-
-int main(void) { puts("\"Programming is like building a multilingual puzzle\n"); return (0); }
+/**
+ * main a program that prints a line with puts function
+ *
+ * return: 0(success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
