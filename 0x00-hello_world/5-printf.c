348d7ef84dbacdd241cb6f5e1db694d871b8068c
Babalo Jara <jarathedawg@gmail.com>
2023-02-23 20:58:34 -0800

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 
-int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); } 
+/**
+ * main -A program that print a line using printf
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
